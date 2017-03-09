01043aa922b38b8f82dd3f7020dce66421083c7e
Vitaly Dub <forceuserz@gmail.com>
2017-03-09 14:06:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default () => ({
         path.replaceWith(
           t.callExpression(
             t.memberExpression(t.identifier('System'), t.identifier('import')),
-            path.node.arguments.concat(t.identifier('__moduleName')),
+            [path.node.arguments[0], t.identifier('__moduleName')],
           ),
         );
       }
