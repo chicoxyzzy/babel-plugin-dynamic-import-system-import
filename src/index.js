66ec5cd8d5b365094148c743421a3c1ed0633b2f
Vitaly Dub <forceuserz@gmail.com>
2017-03-09 12:52:24 +0200

    Add __moduleName argument for relative paths resolution

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default () => ({
         path.replaceWith(
           t.callExpression(
             t.memberExpression(t.identifier('System'), t.identifier('import')),
-            path.node.arguments,
+            path.node.arguments.concat(t.identifier('__moduleName')),
           ),
         );
       }
